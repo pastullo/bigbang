46ad30ffa134c819a8e82fec5b435d04c6028e52
Flavio Bezzeccheri <flacchio@gmail.com>
2014-02-10 18:02:22 +0200

    improved helpers

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -3,9 +3,9 @@ module FriendshipsHelper
 		friendship = current_user.friendships.where(:friend_id => user.id).where(:user_id => current_user.id)
 		# return friendship.inspect
 		if friendship.empty? 
-		    link_to "Down to Bang", friendships_path(:friend_id => user.id), :method => :post
+		    link_to "<span class=\"glyphicon glyphicon-heart\"></span> Down to Bang".html_safe, friendships_path(:friend_id => user.id), :method => :post, class: 'btn btn-sm btn-primary'
 		else
-			link_to "Unbang", friendship.first, :method => :delete
+			link_to "<span class=\"glyphicon glyphicon-remove\"></span> Unbang".html_safe, friendship.first, :method => :delete, class: 'btn btn-sm btn-link'
 	    end		
 	end
 end
