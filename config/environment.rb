7cb9109f54c52250041d74e49bdab9c78ed62f13
pastullo <flacchio@gmail.com>
2014-07-30 21:20:23 +0200

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ ActionMailer::Base.smtp_settings = {
 #     :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
 #     :enable_starttls_auto => true, # detects and uses STARTTLS
 #     :user_name => "tony@5rooms.com",
-#     :password  => "iCiaBpvqZJh64FGW24hOhw", # SMTP password is any valid API key
+#     :password  => "XXXX", # SMTP password is any valid API key
 #     :authentication => 'login', # Mandrill supports 'plain' or 'login'
 #     :domain => 'nadia.africainternetventures.com', # your domain to identify your server when connecting
 #   }
